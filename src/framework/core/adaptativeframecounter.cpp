3cb435a79cb9b18fdf21795dec9be8235526afaf
Renato <mehahx@gmail.com>
2022-06-17 14:55:53 -0300

    fps counter fix

--- src/framework/core/adaptativeframecounter.cpp
+++ src/framework/core/adaptativeframecounter.cpp
@@ -26,8 +26,8 @@ bool AdaptativeFrameCounter::canRefresh()
 {
     if (m_maxFps == 0) return true;
 
-    m_currentTime = stdext::millis();
-    return m_currentTime - m_lastTime >= 1000 / m_maxFps;
+    m_currentTime = stdext::micros();
+    return m_currentTime - m_lastTime >= 1000000 / m_maxFps;
 }
 
 bool AdaptativeFrameCounter::update()
