3b182952868d33ed86046a3a1294eb9b995e6e15
Renato <mehahx@gmail.com>
2022-11-23 14:07:21 -0300

    update draft

--- modules/game_thingeffects/effects.lua
+++ modules/game_thingeffects/effects.lua
@@ -1,5 +1,63 @@
+-- outfit
+-- 307
+-- 293 - raio
+-- effect 32, 12
 EFFECTS = {{
     id = 1,
+    name = 'Teste',
+    thingId = 130,
     category = ThingCategoryEffect,
-    speed = 0.5
+    speed = 0.5,
+    onTop = true,
+    offset = {
+        x = -30,
+        y = -20
+    }, -- Default for All outfit
+    dirsControl = {
+        [North] = {
+            onTop = true,
+            offset = {
+                x = -10,
+                y = 2
+            }
+        },
+        [East] = {
+            x = -11,
+            y = 10
+        },
+        [South] = {
+            x = -12,
+            y = 15
+        },
+        [West] = {
+            x = -13,
+            y = 15
+        }
+    },
+    outfitOffset = { -- Offset based on outfit
+        [300] = { -- Outfit ID
+            offset = {
+                x = -30,
+                y = -20
+            },
+            dirsOffset = {
+                [North] = {
+                    x = -10,
+                    y = 2
+                },
+                [East] = {
+                    x = -11,
+                    y = 10
+                },
+                [South] = {
+                    x = -12,
+                    y = 15
+                },
+                [West] = {
+                    x = -13,
+                    y = 15
+                }
+            }
+        }
+    }
 }}
--- modules/game_thingeffects/thingeffects.lua
+++ modules/game_thingeffects/thingeffects.lua
@@ -1,5 +1,21 @@
 function init()
-    print(EFFECTS)
+
+    connect(ThingEffect, {
+        onAdd = function(effect)
+            if effect:getOwner():isPlayer() and effect:getOwner():getLevel() > 90 then
+                -- effect:addShader(...)
+            end
+        end,
+        onRemove = function(effect)
+        end,
+        onChangeTop = function(effect, onTop)
+            print(onTop)
+        end,
+        onChangeDirection = function(effect, dir)
+            print(dir)
+        end
+    })
+
 end
 
 function terminate()
