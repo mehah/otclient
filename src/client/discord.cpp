9beb39ca203872584e04a041e63966ce728c55e2
Renato <mehahx@gmail.com>
2023-01-11 21:08:37 -0300

    cleanup

--- src/client/discord.cpp
+++ src/client/discord.cpp
@@ -1,12 +1,33 @@
-#include "game.h"
+/*
+ * Copyright (c) 2010-2022 OTClient <https://github.com/edubart/otclient>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include "discord.h"
+
+#include "game.h"
 #include "localplayer.h"
 #include <framework/core/eventdispatcher.h>
 #include <time.h>
-#include <sstream>
 
-static int64_t eptime = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-extern Game g_game;
+const static int64_t EP_TIME = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
 void Discord::Initialize()
 {
@@ -18,41 +39,36 @@ void Discord::Initialize()
 
 void Discord::Update()
 {
-    char* details;
+    std::string info;
     if (g_game.isOnline()) {
-        std::ostringstream level;
-        level << g_game.getLocalPlayer()->getLevel();
-		std::string info = "Adjust in config.h";
-#if SHOW_CHARACTER_NAME_RPC == 1 && SHOW_CHARACTER_LEVEL_RPC == 0 && SHOW_CHARACTER_WORLD_RPC == 0
-		info = "Name: " + g_game.getCharacterName();
-#elif SHOW_CHARACTER_NAME_RPC == 0 && SHOW_CHARACTER_LEVEL_RPC == 1 && SHOW_CHARACTER_WORLD_RPC == 0
-		info = "Level: " + level.str();
-#elif SHOW_CHARACTER_NAME_RPC == 0 && SHOW_CHARACTER_LEVEL_RPC == 0 && SHOW_CHARACTER_WORLD_RPC == 1
-		info = "World: " + g_game.getWorldName();
-#elif SHOW_CHARACTER_NAME_RPC == 1 && SHOW_CHARACTER_LEVEL_RPC == 1 && SHOW_CHARACTER_WORLD_RPC == 0
-		info = "Name: " + g_game.getCharacterName() + " | Level: " + level.str();
-#elif SHOW_CHARACTER_NAME_RPC == 1 && SHOW_CHARACTER_LEVEL_RPC == 0 && SHOW_CHARACTER_WORLD_RPC == 1
-		info = "Name: " + g_game.getCharacterName() + " | World: " + g_game.getWorldName();
-#elif SHOW_CHARACTER_NAME_RPC == 0 && SHOW_CHARACTER_LEVEL_RPC == 1 && SHOW_CHARACTER_WORLD_RPC == 1
-		info = "Level: " + level.str() + " | World: " + g_game.getWorldName();
-#elif SHOW_CHARACTER_NAME_RPC == 1 && SHOW_CHARACTER_LEVEL_RPC == 1 && SHOW_CHARACTER_WORLD_RPC == 1
-        info = g_game.getCharacterName() + " [" + level.str() + "] " + " | World: " + g_game.getWorldName();
+#if SHOW_CHARACTER_NAME_RPC == 1
+        info = "Name: " + g_game.getCharacterName();
 #endif
-        details = const_cast<char*>(info.c_str());
+
+#if SHOW_CHARACTER_LEVEL_RPC == 1
+        const auto& level = std::to_string(g_game.getLocalPlayer()->getLevel());
+        info += info.empty() ? "Level: " + level : "[" + level + "]";
+#endif
+
+#if SHOW_CHARACTER_WORLD_RPC == 1
+        if (!info.empty()) info += " | ";
+        info += "World: " + g_game.getWorldName();
+#endif
+    } else {
+        info = std::string{ OFFLINE_RPC_TEXT };
     }
-    else {
-        std::string notOnlineMsg = OFFLINE_RPC_TEXT;
-        details = const_cast<char*>(notOnlineMsg.c_str());
-    }
+
+    if (info.empty())
+        info = "Adjust in config.h";
 
     DiscordRichPresence discordPresence;
     memset(&discordPresence, 0, sizeof(discordPresence));
     discordPresence.state = STATE_RPC_TEXT;
-    discordPresence.details = details;
-    discordPresence.startTimestamp = eptime;
+    discordPresence.details = info.c_str();
+    discordPresence.startTimestamp = EP_TIME;
     discordPresence.endTimestamp = NULL;
     discordPresence.largeImageKey = RPC_LARGE_IMAGE;
     discordPresence.largeImageText = RPC_LARGE_TEXT;
     Discord_UpdatePresence(&discordPresence);
-    g_dispatcher.scheduleEvent([this] {Update(); }, 30000);
+    g_dispatcher.scheduleEvent([this] { Update(); }, 30000);
 }
