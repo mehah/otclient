d5c773d73c99737621aad43b328afcd84eabcc35
Renato <mehahx@gmail.com>
2023-03-20 19:48:44 -0300

    cleanup

--- src/client/attachedeffect.cpp
+++ src/client/attachedeffect.cpp
@@ -60,10 +60,8 @@ void AttachedEffect::draw(const Point& dest, bool isOnTop, LightView* lightView)
     const int animation = getCurrentAnimationPhase();
     if (m_loop > -1 && animation != m_lastAnimation) {
         m_lastAnimation = animation;
-        if (animation == 0) {
+        if (animation == 0)
             --m_loop;
-            g_logger.info(std::to_string(getLoop()));
-        }
     }
 
     if (m_shader) g_drawPool.setShaderProgram(m_shader, true);
