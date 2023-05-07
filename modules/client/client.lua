2a2cc50af89fa8fe673d84919ce40055092f430f
Joao Pasqualini Costa <joao_pc23@hotmail.com>
2023-05-07 16:20:28 -0300

    Remove duplicate code

--- modules/client/client.lua
+++ modules/client/client.lua
@@ -65,42 +65,6 @@ function init()
         g_sounds.preload(musicFilename)
     end
 
-    -- initialize in fullscreen mode on mobile devices
-    if g_window.getPlatformType() == 'X11-EGL' then
-        g_window.setFullscreen(true)
-    else
-        -- window size
-        local size = {
-            width = 800,
-            height = 600
-        }
-        size = g_settings.getSize('window-size', size)
-        g_window.resize(size)
-
-        -- window position, default is the screen center
-        local displaySize = g_window.getDisplaySize()
-        local defaultPos = {
-            x = (displaySize.width - size.width) / 2,
-            y = (displaySize.height - size.height) / 2
-        }
-        local pos = g_settings.getPoint('window-pos', defaultPos)
-        pos.x = math.max(pos.x, 0)
-        pos.y = math.max(pos.y, 0)
-        g_window.move(pos)
-
-        -- window maximized?
-        local maximized = g_settings.getBoolean('window-maximized', false)
-        if maximized then
-            g_window.maximize()
-        end
-    end
-
-    g_window.setTitle(g_app.getName())
-    g_window.setIcon('/images/clienticon')
-
-    -- poll resize events
-    g_window.poll()
-
     -- generate machine uuid, this is a security measure for storing passwords
     if not g_crypt.setMachineUUID(g_settings.get('uuid')) then
         g_settings.set('uuid', g_crypt.getMachineUUID())
