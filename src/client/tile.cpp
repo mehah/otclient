5e62099afea8eaf4971763f259d8aca826240509
Renato Foot <renato@foot.br>
2023-12-08 00:37:41 +0000

    fix: wrong rebase change accepted
    Signed-off-by: Renato Foot <renato@foot.br>

--- src/client/tile.cpp
+++ src/client/tile.cpp
@@ -87,7 +87,7 @@ void Tile::draw(const Point& dest, const MapPosInfo& mapRect, int flags, bool is
     for (const auto& thing : m_things) {
         thing->drawWidgets(mapRect);
     }
-    
+
     drawWidgets(mapRect);
 }
 
@@ -167,7 +167,8 @@ void Tile::clean()
 #endif
         ) {
         g_dispatcher.scheduleEvent([tile = static_self_cast<Tile>()] {
-            g_ui.getMapWidget()->getMapView()->removeForegroundTile(tile);
+            if (g_client.getMapWidget())
+                g_client.getMapWidget()->getMapView()->removeForegroundTile(tile);
         }, g_game.getServerBeat());
     }
 
@@ -859,7 +860,8 @@ void Tile::setText(const std::string& text, Color color)
     if (!m_text) {
         m_text = std::make_shared<StaticText>();
         g_dispatcher.scheduleEvent([tile = static_self_cast<Tile>()] {
-            g_ui.getMapWidget()->getMapView()->addForegroundTile(tile);
+            if (g_ui.getMapWidget())
+                g_ui.getMapWidget()->getMapView()->addForegroundTile(tile);
         }, g_game.getServerBeat());
     }
 
@@ -882,7 +884,8 @@ void Tile::setTimer(int time, Color color)
     if (!m_timerText) {
         m_timerText = std::make_shared<StaticText>();
         g_dispatcher.scheduleEvent([tile = static_self_cast<Tile>()] {
-            g_ui.getMapWidget()->getMapView()->addForegroundTile(tile);
+            if (g_ui.getMapWidget())
+                g_ui.getMapWidget()->getMapView()->addForegroundTile(tile);
         }, g_game.getServerBeat());
     }
 
