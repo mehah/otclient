72744edc3b9913b920e0fd12e929604f682fda75
kokekanon <114332266+kokekanon@users.noreply.github.com>
2023-12-15 09:54:39 -0300

    fix: Move setEffectAlpha and setMissileAlpha from "framework" to "client" (luafunctions) (#677)
    * feat: Opacity effect and missile in options
    
    issue of "roriscrave" in otland
    
    * OTC naming convention
    
    OTC naming convention
    
    * test review conde2
    
    - with bug (opacity in tile)
    
    * fix: capital letter
    
    * line
    
    * Update modules/client_options/general.otui
    
    Co-authored-by: Caio Santoro dos Santos <61211407+csantbr@users.noreply.github.com>
    
    * Fix: Opacity tile
    
    * fix: Move setEffectAlpha and setMissileAlpha from framework to client
    
    * Revert "fix: Move setEffectAlpha and setMissileAlpha from framework to client"
    
    This reverts commit e58a883927a1584c4ae15c2f2a0cda4a172cd79b.
    
    * Revert "Revert "fix: Move setEffectAlpha and setMissileAlpha from framework to client""
    
    This reverts commit ed3cd148c36468f9fc5fd7e5153e19934cdc7e06.
    
    * fix: line
    
    ---------
    
    Co-authored-by: Caio Santoro dos Santos <61211407+csantbr@users.noreply.github.com>

--- src/framework/luafunctions.cpp
+++ src/framework/luafunctions.cpp
@@ -31,7 +31,6 @@
 #include <framework/platform/platform.h>
 #include <framework/stdext/net.h>
 #include <framework/util/crypt.h>
-#include <client/client.h>
 
 #ifdef FRAMEWORK_GRAPHICS
 #include "framework/graphics/particleeffect.h"
@@ -324,11 +323,6 @@ void Application::registerLuaFunctions()
     g_lua.bindSingletonFunction("g_app", "setAnimatedTextScale", &GraphicalApplication::setAnimatedTextScale, &g_app);
     g_lua.bindSingletonFunction("g_app", "setStaticTextScale", &GraphicalApplication::setStaticTextScale, &g_app);
     
-    // Client
-    g_lua.registerSingletonClass("g_client");
-    g_lua.bindSingletonFunction("g_client", "setEffectAlpha", &Client::setEffectAlpha, &g_client);
-    g_lua.bindSingletonFunction("g_client", "setMissileAlpha", &Client::setMissileAlpha, &g_client);
-
     // PlatformWindow
     g_lua.registerSingletonClass("g_window");
     g_lua.bindSingletonFunction("g_window", "move", &PlatformWindow::move, &g_window);
