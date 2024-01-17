ef8069cf0fc506deddd13e7638246e24d577402c
SkullzOTS <86809689+SkullzOTS@users.noreply.github.com>
2024-01-17 19:19:55 -0300

    Update gameinterface.lua

--- modules/game_interface/gameinterface.lua
+++ modules/game_interface/gameinterface.lua
@@ -1,5 +1,3 @@
-WALK_STEPS_RETRY = 10
-
 gameRootPanel = nil
 gameMapPanel = nil
 gameRightPanel = nil
@@ -18,12 +16,7 @@ exitWindow = nil
 bottomSplitter = nil
 limitedZoom = false
 currentViewMode = 0
-smartWalkDirs = {}
-smartWalkDir = nil
-firstStep = false
 hookedMenuOptions = {}
-lastDirTime = g_clock.millis()
-lastManualWalk = 0
 
 function init()
     g_ui.importStyle('styles/countwindow')
@@ -56,7 +49,6 @@ function init()
     gameRootPanel:hide()
     gameRootPanel:lower()
     gameRootPanel.onGeometryChange = updateStretchShrink
-    gameRootPanel.onFocusChange = stopSmartWalk
 
     mouseGrabberWidget = gameRootPanel:getChildById('mouseGrabber')
     mouseGrabberWidget.onMouseRelease = onMouseGrabberRelease
@@ -122,30 +114,6 @@ function onSelectPanel(self, checked)
 end
 
 function bindKeys()
-    gameRootPanel:setAutoRepeatDelay(200)
-
-    bindWalkKey('Up', North)
-    bindWalkKey('Right', East)
-    bindWalkKey('Down', South)
-    bindWalkKey('Left', West)
-    bindWalkKey('Numpad8', North)
-    bindWalkKey('Numpad9', NorthEast)
-    bindWalkKey('Numpad6', East)
-    bindWalkKey('Numpad3', SouthEast)
-    bindWalkKey('Numpad2', South)
-    bindWalkKey('Numpad1', SouthWest)
-    bindWalkKey('Numpad4', West)
-    bindWalkKey('Numpad7', NorthWest)
-
-    bindTurnKey('Ctrl+Up', North)
-    bindTurnKey('Ctrl+Right', East)
-    bindTurnKey('Ctrl+Down', South)
-    bindTurnKey('Ctrl+Left', West)
-    bindTurnKey('Ctrl+Numpad8', North)
-    bindTurnKey('Ctrl+Numpad6', East)
-    bindTurnKey('Ctrl+Numpad2', South)
-    bindTurnKey('Ctrl+Numpad4', West)
-
     g_keyboard.bindKeyPress('Escape', function()
         g_game.cancelAttackAndFollow()
     end, gameRootPanel)
@@ -171,41 +139,6 @@ function bindKeys()
     end
 end
 
-function bindWalkKey(key, dir)
-    g_keyboard.bindKeyDown(key, function()
-        onWalkKeyDown(dir)
-    end, gameRootPanel, true)
-    g_keyboard.bindKeyUp(key, function()
-        changeWalkDir(dir, true)
-    end, gameRootPanel, true)
-    g_keyboard.bindKeyPress(key, function()
-        smartWalk(dir)
-    end, gameRootPanel)
-end
-
-function unbindWalkKey(key)
-    g_keyboard.unbindKeyDown(key, gameRootPanel)
-    g_keyboard.unbindKeyUp(key, gameRootPanel)
-    g_keyboard.unbindKeyPress(key, gameRootPanel)
-end
-
-function bindTurnKey(key, dir)
-    local function callback(widget, code, repeatTicks)
-        if g_clock.millis() - lastDirTime >= modules.client_options.getOption('turnDelay') then
-            g_game.turn(dir)
-            changeWalkDir(dir)
-
-            lastDirTime = g_clock.millis()
-        end
-    end
-
-    g_keyboard.bindKeyPress(key, callback, gameRootPanel)
-end
-
-function unbindTurnKey(key)
-    g_keyboard.unbindKeyPress(key, gameRootPanel)
-end
-
 function terminate()
     hide()
     if g_app.hasUpdater() then
@@ -222,8 +155,6 @@ function terminate()
     })
 
     hookedMenuOptions = {}
-
-    stopSmartWalk()
 
     disconnect(g_game, {
         onGameStart = onGameStart,
@@ -250,13 +181,6 @@ end
 
 function onGameStart()
     show()
-
-    -- open tibia has delay in auto walking
-    if not g_game.isOfficialTibia() then
-        g_game.enableFeature(GameForceFirstAutoWalkStep)
-    else
-        g_game.disableFeature(GameForceFirstAutoWalkStep)
-    end
 end
 
 function onGameEnd()
@@ -439,66 +363,6 @@ function tryLogout(prompt)
     else
         yesCallback()
     end
-end
-
-function stopSmartWalk()
-    smartWalkDirs = {}
-    smartWalkDir = nil
-end
-
-function onWalkKeyDown(dir)
-    if modules.client_options.getOption('autoChaseOverride') then
-        if g_game.isAttacking() and g_game.getChaseMode() == ChaseOpponent then
-            g_game.setChaseMode(DontChase)
-        end
-    end
-    firstStep = true
-    changeWalkDir(dir)
-end
-
-function changeWalkDir(dir, pop)
-    while table.removevalue(smartWalkDirs, dir) do
-    end
-    if pop then
-        if #smartWalkDirs == 0 then
-            stopSmartWalk()
-            return
-        end
-    else
-        table.insert(smartWalkDirs, 1, dir)
-    end
-
-    smartWalkDir = smartWalkDirs[1]
-    if modules.client_options.getOption('smartWalk') and #smartWalkDirs > 1 then
-        for _, d in pairs(smartWalkDirs) do
-            if (smartWalkDir == North and d == West) or (smartWalkDir == West and d == North) then
-                smartWalkDir = NorthWest
-                break
-            elseif (smartWalkDir == North and d == East) or (smartWalkDir == East and d == North) then
-                smartWalkDir = NorthEast
-                break
-            elseif (smartWalkDir == South and d == West) or (smartWalkDir == West and d == South) then
-                smartWalkDir = SouthWest
-                break
-            elseif (smartWalkDir == South and d == East) or (smartWalkDir == East and d == South) then
-                smartWalkDir = SouthEast
-                break
-            end
-        end
-    end
-end
-
-function smartWalk(dir)
-    if g_keyboard.getModifiers() ~= KeyboardNoModifier then
-        return false
-    end
-
-    local dire = smartWalkDir or dir
-    g_game.walk(dire, firstStep)
-    firstStep = false
-
-    lastManualWalk = g_clock.millis()
-    return true
 end
 
 function updateStretchShrink()
