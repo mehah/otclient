5ce56c713bbccd782ac8f66d9ac23a56045b2163
kokekanon <114332266+kokekanon@users.noreply.github.com>
2024-07-31 20:29:07 -0400

    fix: purse buttom on inventory (#835)

--- modules/game_inventory/inventory.lua
+++ modules/game_inventory/inventory.lua
@@ -249,6 +249,7 @@ function inventoryController:onGameStart()
             end
         end
     end
+    inventoryController.ui.onPanel.purseButton:setVisible(g_game.getFeature(GamePurseSlot))
 end
 
 function onSetSafeFight(self, checked)
