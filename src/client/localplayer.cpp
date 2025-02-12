e6c66899c00d2c1ba311d67d7bb1e629f2a90825
Renato Machado <mehahx@gmail.com>
2025-02-12 18:06:52 -0300

    Update localplayer.cpp

--- src/client/localplayer.cpp
+++ src/client/localplayer.cpp
@@ -76,17 +76,15 @@ void LocalPlayer::preWalk(Otc::Direction direction)
     const auto& oldPos = getPosition();
     Creature::walk(oldPos, m_preWalks.emplace_back(oldPos.translatedToDirection(direction)));
 
-    if (g_game.getFeature(Otc::GameLatencyAdaptiveCamera)) {
-        cancelAjustInvalidPosEvent();
+    cancelAjustInvalidPosEvent();
 
-        int duration = std::max<int>(getStepDuration(), g_game.getPing()) + 50;
-        duration = std::clamp<int>(duration, 500, 1000); // Ensures it does not exceed 1000ms
+    int duration = std::max<int>(getStepDuration(), g_game.getPing()) + 50;
+    duration = std::clamp<int>(duration, 500, 1000); // Ensures it does not exceed 1000ms
 
-        m_ajustInvalidPosEvent = g_dispatcher.scheduleEvent([this, self = asLocalPlayer()] {
-            m_preWalks.clear();
-            m_ajustInvalidPosEvent = nullptr;
-        }, duration);
-    }
+    m_ajustInvalidPosEvent = g_dispatcher.scheduleEvent([this, self = asLocalPlayer()] {
+        m_preWalks.clear();
+        m_ajustInvalidPosEvent = nullptr;
+    }, duration);
 }
 
 void LocalPlayer::cancelAjustInvalidPosEvent() {
