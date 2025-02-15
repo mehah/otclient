3810190de33b56bae85f7574af07c72a0a52959b
Renato Machado <mehahx@gmail.com>
2025-02-14 21:23:27 -0300

    fix #1099

--- src/client/creature.cpp
+++ src/client/creature.cpp
@@ -107,7 +107,7 @@ void Creature::drawLight(const Point& dest, const LightViewPtr& lightView) {
 
 void Creature::draw(const Rect& destRect, const uint8_t size, const bool center)
 {
-    if (!getThingType())
+    if (!canDraw())
         return;
 
     uint8_t frameSize = getExactSize();
@@ -1127,6 +1127,9 @@ void Creature::onStartDetachEffect(const AttachedEffectPtr& effect) {
 }
 
 void Creature::setStaticWalking(const uint16_t v) {
+    if (!canDraw())
+        return;
+
     if (m_walkUpdateEvent) {
         m_walkUpdateEvent->cancel();
         m_walkUpdateEvent = nullptr;
