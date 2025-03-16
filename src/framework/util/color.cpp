c90f99b3a6ca671313300f6c3b581385ce7c15bf
Renato Machado <mehahx@gmail.com>
2025-03-16 13:10:51 -0300

    uodate

--- src/framework/util/color.cpp
+++ src/framework/util/color.cpp
@@ -54,7 +54,7 @@ Color::Color(const std::string_view coltext)
     update();
 }
 
-constexpr void Color::update() { m_hash = stdext::hash_int(rgba()); }
+constexpr void Color::update() { m_hash = rgba(); }
 
 std::ostream& operator<<(std::ostream& out, const Color& color)
 {
