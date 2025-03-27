9aaeaa26a181a490f5660b4dd687b4d3a8a2f18a
kokekanon <114332266+kokekanon@users.noreply.github.com>
2025-03-27 17:46:27 -0300

    feat: API Create accounts MyACC

--- init.lua
+++ init.lua
@@ -6,6 +6,8 @@ Services = {
     --updater = "http://localhost/api/updater.php", --./updater
     --status = "http://localhost/login.php", --./client_entergame | ./client_topmenu
     --websites = "http://localhost/?subtopic=accountmanagement", --./client_entergame "Forgot password and/or email"
+    --createAccount = "http://localhost/clientcreateaccount.php", --./client_entergame -- createAccount.lua
+
 }
 
 --[[
