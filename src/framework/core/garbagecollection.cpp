78baea41002a6066e4629cd8e05df067d774f381
Renato Machado <mehahx@gmail.com>
2025-05-26 20:12:31 -0300

    Update garbagecollection.cpp

--- src/framework/core/garbagecollection.cpp
+++ src/framework/core/garbagecollection.cpp
@@ -58,7 +58,7 @@ void GarbageCollection::drawpoll() {
 void GarbageCollection::texture() {
     static constexpr uint32_t IDLE_TIME = 25 * 60 * 1000; // 25min
 
-    std::unique_lock l(g_textures.m_mutex);
+    std::scoped_lock l(g_textures.m_mutex, g_drawPool.get(DrawPoolType::MAP)->getMutex(), g_drawPool.get(DrawPoolType::FOREGROUND)->getMutex());
 
     std::erase_if(g_textures.m_textures, [](const auto& item) {
         const auto& [key, tex] = item;
