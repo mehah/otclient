d72f165242d99f7c848f062d7bdf2d6d55203892
Renato Machado <mehahx@gmail.com>
2025-06-26 17:12:05 -0300

    Update Copyright

--- src/client/animator.h
+++ src/client/animator.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2010-2024 OTClient <https://github.com/edubart/otclient>
+* Copyright (c) 2010-2025 OTClient <https://github.com/edubart/otclient>
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
