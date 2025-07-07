1c9e857c8ff789dd745c616ae56de35313c1d615
Renato Machado <mehahx@gmail.com>
2025-07-07 19:35:28 -0300

    improve: refactor `things.lua` to Controller System and add feature fallback logic for `loadDat` (#1224)
    fix #1221

--- modules/game_things/things.lua
+++ modules/game_things/things.lua
@@ -1,17 +1,7 @@
-filename = nil
-loaded = false
+ThingsLoaderController = Controller:new()
 
-function init()
-    connect(g_game, {
-        onClientVersionChange = load
-    })
-end
-
-function terminate()
-    disconnect(g_game, {
-        onClientVersionChange = load
-    })
-end
+local filename = nil
+local loaded = false
 
 function setFileName(name)
     filename = name
@@ -21,7 +11,37 @@ function isLoaded()
     return loaded
 end
 
-function load(version)
+local function tryLoadDatWithFallbacks(datPath)
+    if g_things.loadDat(datPath) then
+        return true
+    end
+
+    local featureFlags = {
+        GameSpritesU32,
+        GameEnhancedAnimations,
+        GameIdleAnimations
+    }
+
+    local combinations = {
+        { 1 }, { 2 }, { 3 },
+        { 1, 2 }, { 1, 3 }, { 2, 3 },
+        { 1, 2, 3 }
+    }
+
+    for _, combo in ipairs(combinations) do
+        for _, idx in ipairs(combo) do
+            g_game.enableFeature(featureFlags[idx])
+        end
+
+        if g_things.loadDat(datPath) then
+            return true
+        end
+    end
+
+    return false
+end
+
+local function load(version)
     local errorList = {}
 
     if version >= 1281 and not g_game.getFeature(GameLoadSprInsteadProtobuf) then
@@ -42,16 +62,20 @@ function load(version)
             sprPath = resolvepath('/data/things/' .. version .. '/Tibia')
         end
 
-        if not g_things.loadDat(datPath) then
+        g_logger.setLevel(5)
+        if not tryLoadDatWithFallbacks(datPath) then
             errorList[#errorList + 1] = tr('Unable to load dat file, please place a valid dat in \'%s.dat\'', datPath)
         end
+        g_logger.setLevel(1)
+
         if not g_sprites.loadSpr(sprPath) then
             errorList[#errorList + 1] = tr('Unable to load spr file, please place a valid spr in \'%s.spr\'', sprPath)
         end
         if g_game.getFeature(GameLoadSprInsteadProtobuf) and version >= 1281 then
             local staticPath = resolvepath(string.format('/things/%d/appearances', version))
             if not g_things.loadAppearances(staticPath) then
-                g_logger.warning(string.format("[game_things.load()] Couldn't load /things/%d/appearances.dat, possible packets error.", version))
+                g_logger.warning(string.format(
+                    "[game_things.load()] Couldn't load /things/%d/appearances.dat, possible packets error.", version))
             end
         end
     end
@@ -65,19 +89,18 @@ function load(version)
         return
     end
 
-    -- loading client files failed, show an error
     local messageBox = displayErrorBox(tr('Error'), table.concat(errorList, "\n"))
     addEvent(function()
         messageBox:raise()
         messageBox:focus()
     end)
 
-    disconnect(g_game, {
-        onClientVersionChange = load
-    })
     g_game.setClientVersion(0)
     g_game.setProtocolVersion(0)
-    connect(g_game, {
+end
+
+function ThingsLoaderController:onInit()
+    self:registerEvents(g_game, {
         onClientVersionChange = load
     })
 end
