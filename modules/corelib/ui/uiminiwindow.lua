1e136935841d4442a28c7d665a4fc1b73b94020c
LIBERGOD <37589902+libergod@users.noreply.github.com>
2025-08-07 11:27:16 -0700

    feat: Enhance miniwindow UI with new buttons and improved layout, including toggleFilter and contextMenu buttons

--- modules/corelib/ui/uiminiwindow.lua
+++ modules/corelib/ui/uiminiwindow.lua
@@ -41,6 +41,12 @@ function UIMiniWindow:minimize(dontSave)
     self.maximizedHeight = self:getHeight()
     self:setHeight(self.minimizedHeight)
 
+    -- Hide miniborder when minimizing
+    local miniborder = self:recursiveGetChildById('miniborder')
+    if miniborder then
+        miniborder:setVisible(false)
+    end
+
     if not dontSave then
         self:setSettings({
             minimized = true
@@ -57,6 +63,12 @@ function UIMiniWindow:maximize(dontSave)
     self:getChildById('bottomResizeBorder'):show()
     self:getChildById('minimizeButton'):setOn(false)
     self:setHeight(self:getSettings('height') or self.maximizedHeight)
+
+    -- Show miniborder when maximizing
+    local miniborder = self:recursiveGetChildById('miniborder')
+    if miniborder then
+        miniborder:setVisible(true)
+    end
 
     if not dontSave then
         self:setSettings({
