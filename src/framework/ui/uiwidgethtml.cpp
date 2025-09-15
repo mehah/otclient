436fa5534626a879806d106e0679453b17557071
Renato Machado <mehahx@gmail.com>
2025-09-15 00:56:20 -0300

    fix position

--- src/framework/ui/uiwidgethtml.cpp
+++ src/framework/ui/uiwidgethtml.cpp
@@ -824,7 +824,7 @@ void UIWidget::applyAnchorAlignment() {
                 anchored = true;
                 addAnchor(Fw::AnchorVerticalCenter, "parent", Fw::AnchorVerticalCenter);
             }
-        }
+        } else anchored = false;
 
         if (anchored) {
             if (!ctx.prevNonFloat) {
