282cc8b1ab3fb33784fcb5c91efaacc72b4ee53a
Renato Machado <mehahx@gmail.com>
2025-09-28 14:21:02 -0300

    update sample

--- modules/game_htmlsample/htmlsample.lua
+++ modules/game_htmlsample/htmlsample.lua
@@ -1,66 +1,41 @@
 HtmlSample = Controller:new()
-
-
-local cores = {
-    "red",
-    "blue",
-    "green",
-    "darkorange",
-    "purple",
-    "darkred",
-    "teal",
-    "navy",
-    "maroon",
-    "darkgreen",
-    "brown",
-    "darkslategray",
-    "crimson",
-    "darkviolet",
-    "firebrick",
-    "midnightblue",
-    "sienna",
-    "darkolivegreen",
-    "indigo",
-    "darkslateblue",
-}
-
 function HtmlSample:onInit()
     self:loadHtml('htmlsample.html')
+    self:equalizerEffect()
+end
 
-    for r = 1, 2 do
-        for i = 1, #cores do
-            local cor = cores[math.random(#cores)]
-            local largura = math.random(10, 50)
-            self.ui:append(string.format(
-                [[
-                    <style>
-                        div {
-                            background-color: %s;
-                            display: inline-block;
-                        }
-                    </style>
-                    <div style="width: %dpx; height: 50px;"></div>
-                ]], cor, largura
-            ))
-        end
+function HtmlSample:addPlayer(name)
+    self:findWidget('#players'):append(string.format([[
+        <div>%s</div>
+    ]], name))
+end
+
+function HtmlSample:equalizerEffect()
+    local widgets = self:findWidgets('.line')
+
+    for _, widget in pairs(widgets) do
+        local minV = math.random(0, 30)
+        local maxV = math.random(70, 100)
+        if minV > maxV then minV, maxV = maxV, minV end
+
+        local range = maxV - minV
+        local speed = math.max(1, math.floor(range / 20)) + math.random(0, 1)
+
+        local value = math.random(minV, maxV)
+        local dir   = (math.random(0, 1) == 0) and -1 or 1
+
+        self:cycleEvent(function()
+            value = value + dir * speed
+            if value >= maxV then
+                value = maxV
+                dir = -1
+            elseif value <= minV then
+                value = minV
+                dir = 1
+            end
+
+            widget:setHeight(10 + value)
+            widget:setTop(89 - value)
+        end, 30)
     end
-
-
-
-    local i = 0
-    local sum = true
-    self:cycleEvent(function()
-        self.ui:setWidth(10 + i)
-        if i == 1000 then
-            sum = false
-        elseif i == 100 then
-            sum = true
-        end
-
-        if sum then
-            i = i + 25
-        else
-            i = i - 25
-        end
-    end, 60)
 end
