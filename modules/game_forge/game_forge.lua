e70faf7fb173965fdd3e0a4fcbb003d00776d4f9
Gabriel Alcântara Bernardes <gabrielalcantaratp@gmail.com>
2025-10-02 21:15:42 -0300

    Show forge resources from player balances

--- modules/game_forge/game_forge.lua
+++ modules/game_forge/game_forge.lua
@@ -20,6 +20,107 @@ ui = {
 forgeController = Controller:new()
 local forgeButton
 
+local forgeResourceTypes = {
+    dust = ResourceTypes and ResourceTypes.FORGE_DUST or 70,
+    sliver = ResourceTypes and ResourceTypes.FORGE_SLIVER or 71,
+    cores = ResourceTypes and ResourceTypes.FORGE_CORES or 72
+}
+
+local function defaultResourceFormatter(value)
+    local numericValue = tonumber(value) or 0
+    return tostring(numericValue)
+end
+
+local function formatGoldAmount(value)
+    local numericValue = tonumber(value) or 0
+    if type(comma_value) == 'function' then
+        return comma_value(tostring(numericValue))
+    end
+
+    return tostring(numericValue)
+end
+
+local function formatDustAmount(value)
+    local numericValue = tonumber(value) or 0
+    return string.format('%d/100', numericValue)
+end
+
+local forgeStatusConfigs = {
+    {
+        selector = '#forgeGoldAmount',
+        formatter = formatGoldAmount,
+        getValue = function(player)
+            if not player then
+                return 0
+            end
+
+            return player:getTotalMoney() or 0
+        end,
+        eventResourceTypes = {
+            ResourceTypes and ResourceTypes.BANK_BALANCE or 0,
+            ResourceTypes and ResourceTypes.GOLD_EQUIPPED or 1
+        }
+    },
+    {
+        selector = '#forgeDustAmount',
+        formatter = formatDustAmount,
+        resourceType = forgeResourceTypes.dust
+    },
+    {
+        selector = '#forgeSliverAmount',
+        formatter = defaultResourceFormatter,
+        resourceType = forgeResourceTypes.sliver
+    },
+    {
+        selector = '#forgeCoreAmount',
+        formatter = defaultResourceFormatter,
+        resourceType = forgeResourceTypes.cores
+    }
+}
+
+local forgeResourceConfig = {}
+
+local function registerResourceConfig(resourceType, config)
+    if not resourceType or forgeResourceConfig[resourceType] == config then
+        return
+    end
+
+    forgeResourceConfig[resourceType] = config
+end
+
+for _, config in ipairs(forgeStatusConfigs) do
+    if config.resourceType then
+        registerResourceConfig(config.resourceType, config)
+    end
+
+    if config.eventResourceTypes then
+        for _, resourceType in ipairs(config.eventResourceTypes) do
+            registerResourceConfig(resourceType, config)
+        end
+    end
+end
+
+local function resolveStatusWidget(controller, config)
+    if config.widget then
+        if config.widget:isDestroyed() then
+            config.widget = nil
+        else
+            return config.widget
+        end
+    end
+
+    if not controller.ui then
+        return nil
+    end
+
+    local widget = controller:findWidget(config.selector)
+    if widget then
+        config.widget = widget
+    end
+
+    return widget
+end
+
 local function loadTabFragment(tabName)
     local fragment = io.content(('modules/%s/tab/%s/%s.html'):format(forgeController.name, tabName, tabName))
     local container = forgeController.ui.content:prepend(fragment)
@@ -52,6 +153,9 @@ end
 local function toggle(self)
     g_game.forgeRequest()
     forgeController:loadHtml('game_forge.html')
+    for _, config in ipairs(forgeStatusConfigs) do
+        config.widget = nil
+    end
     g_ui.importStyle("otui/style.otui")
     self.modeFusion, self.modeTransfer = false, false
 
@@ -70,6 +174,43 @@ local function toggle(self)
     end
 
     SelectWindow("fusionMenu")
+    self:updateResourceBalances()
+end
+
+local function updateStatusConfig(controller, config, player)
+    local widget = resolveStatusWidget(controller, config)
+    if not widget then
+        return
+    end
+
+    local amount = 0
+    if config.getValue then
+        amount = config.getValue(player) or 0
+    elseif player and config.resourceType then
+        amount = player:getResourceBalance(config.resourceType) or 0
+    end
+
+    local formatter = config.formatter or defaultResourceFormatter
+    widget:setText(formatter(amount))
+end
+
+function forgeController:updateResourceBalances(resourceType)
+    if not self.ui then
+        return
+    end
+
+    local player = g_game.getLocalPlayer()
+
+    if resourceType then
+        local config = forgeResourceConfig[resourceType]
+        if config then
+            updateStatusConfig(self, config, player)
+        end
+    else
+        for _, config in ipairs(forgeStatusConfigs) do
+            updateStatusConfig(self, config, player)
+        end
+    end
 end
 
 function forgeController:onInit()
@@ -77,6 +218,14 @@ function forgeController:onInit()
         forgeButton = modules.game_mainpanel.addToggleButton('forgeButton', tr('Open Exaltation Forge'),
             '/images/options/button-exaltation-forge.png', function() toggle(self) end)
     end
+
+    self:registerEvents(g_game, {
+        onResourcesBalanceChange = function(_, _, resourceType)
+            if forgeResourceConfig[resourceType] then
+                self:updateResourceBalances(resourceType)
+            end
+        end
+    })
 end
 
 function SelectWindow(type, isBackButtonPress)
