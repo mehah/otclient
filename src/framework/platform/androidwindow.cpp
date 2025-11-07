641127615bb65961448d063adee33c29189a1f30
SkullzOTS <86809689+SkullzOTS@users.noreply.github.com>
2025-11-07 19:21:53 -0300

    fix build

--- src/framework/platform/androidwindow.cpp
+++ src/framework/platform/androidwindow.cpp
@@ -25,6 +25,7 @@
 #include "androidwindow.h"
 #include "androidmanager.h"
 #include <game-activity/native_app_glue/android_native_app_glue.h>
+#include "framework/core/clock.h"
 #include <framework/core/eventdispatcher.h>
 
 AndroidWindow& g_androidWindow = (AndroidWindow&) g_window;
