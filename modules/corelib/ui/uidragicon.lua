cd84933cae02d65d3e8ff29978522132c9afb5a3
kokekanon <114332266+kokekanon@users.noreply.github.com>
2025-11-20 21:27:50 -0300

    fix: leak

--- modules/corelib/ui/uidragicon.lua
+++ modules/corelib/ui/uidragicon.lua
@@ -3,21 +3,32 @@ local uiIcon = nil
 
 function UIDragIcon:display(item)
     if modules.client_options.getOption('showDragIcon') then
-        uiIcon = g_ui.createWidget('UIDragIcon', rootWidget)
+        if uiIcon == nil then
+            uiIcon = g_ui.createWidget('UIDragIcon', rootWidget)
+            uiIcon:setVirtual(true)
+            uiIcon:setShowCount(false)
+        end
+        
         uiIcon:setItem(item)
-        uiIcon:setVirtual(true)
-        uiIcon:setShowCount(false)
         uiIcon:show()
+        
+        connect(rootWidget, { onMouseMove = onMouseMove })
     end
-    
-    connect(rootWidget, { onMouseMove = onMouseMove })
 end
 
 function UIDragIcon:hide()
     if uiIcon ~= nil then
         uiIcon:hide()
+        disconnect(rootWidget, { onMouseMove = onMouseMove })
     end
-    disconnect(rootWidget, { onMouseMove = onMouseMove })
+end
+
+function UIDragIcon:destroy()
+    if uiIcon ~= nil then
+        disconnect(rootWidget, { onMouseMove = onMouseMove })
+        uiIcon:destroy()
+        uiIcon = nil
+    end
 end
 
 function onMouseMove(self, mousePos, mouseMoved)
