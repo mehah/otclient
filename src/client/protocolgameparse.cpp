caf9d8674d7cf5fdf969138080eb14de889e24dd
divinity76 <hans@loltek.net>
2025-12-16 14:46:32 +0100

    oops

--- src/client/protocolgameparse.cpp
+++ src/client/protocolgameparse.cpp
@@ -2320,7 +2320,7 @@ void ProtocolGame::parsePlayerStats(const InputMessagePtr& msg) const
     const uint32_t health = g_game.getFeature(Otc::GameDoubleHealth) ? msg->getU32() : msg->getU16();
     const uint32_t maxHealth = g_game.getFeature(Otc::GameDoubleHealth) ? msg->getU32() : msg->getU16();
     uint32_t freeCapacity = g_game.getFeature(Otc::GameDoubleFreeCapacity) ? msg->getU32() : msg->getU16();
-    if (g_game.getClientVersion() >= 772) {
+    if (g_game.getClientVersion() > 772) {
         // todo: We only know scaling started some time after 7.72; the 772 cutoff is a placeholder until we find the exact version.
         freeCapacity /= 100;
     }
