5c3938281804f3131f19019bf0bf6fa2f124a32f
LIBERGOD <37589902+libergod@users.noreply.github.com>
2026-01-03 16:24:45 -0800

    Revert "fix issues with Convergence flag"
    This reverts commit 8b9662179eed111d8e2c4ea764ad73a3379d9349.

--- modules/game_forge/game_forge.lua
+++ modules/game_forge/game_forge.lua
@@ -554,7 +554,6 @@ ForgeController.fusion = {
 
     clip = { x = 0, y = 0, width = 116, height = 34 },
     items = {},
-    convergenceItems = {},
     selected = cloneValue(ForgeController.baseSelected),
     selectedTarget = cloneValue(ForgeController.baseSelected),
     canFusion = false,
