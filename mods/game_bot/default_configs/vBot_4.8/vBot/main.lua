a62893feb8d9bdc7d6695a54417ebe88b397e0a4
divinity76 <hans@loltek.net>
2026-01-05 05:14:21 +0100

    Remove vBot upstream version check
    vBot upstream has been abandoned, last commit was in 2022, PR's are being ignored (for example https://github.com/Vithrax/vBot/pull/18 has been ignored since 2024)
    
    and the check may cause network failures.
    
    resolve https://github.com/mehah/otclient/issues/1544

--- mods/game_bot/default_configs/vBot_4.8/vBot/main.lua
+++ mods/game_bot/default_configs/vBot_4.8/vBot/main.lua
@@ -2,25 +2,6 @@ local version = "4.8"
 local currentVersion
 local available = false
 
-storage.checkVersion = storage.checkVersion or 0
-
--- check max once per 12hours
-if os.time() > storage.checkVersion + (12 * 60 * 60) then
-
-    storage.checkVersion = os.time()
-
-    HTTP.get("https://raw.githubusercontent.com/Vithrax/vBot/main/vBot/version.txt", function(data, err)
-        if err then
-          warn("[vBot updater]: Unable to check version:\n" .. err)
-          return
-        end
-
-        currentVersion = data
-        available = true
-    end)
-
-end
-
 UI.Label("vBot v".. version .." \n Vithrax#5814")
 UI.Button("Official OTCv8 Discord!", function() g_platform.openUrl("https://discord.gg/yhqBE4A") end)
 UI.Separator()
