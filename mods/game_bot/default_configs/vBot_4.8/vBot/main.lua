cf757badbb7fa519c794efba77da109d3405daa1
divinity76 <hans@loltek.net>
2026-01-05 05:30:37 +0100

    forgot some variables. must be tired (5am...)

--- mods/game_bot/default_configs/vBot_4.8/vBot/main.lua
+++ mods/game_bot/default_configs/vBot_4.8/vBot/main.lua
@@ -1,6 +1,4 @@
 local version = "4.8"
-local currentVersion
-local available = false
 
 UI.Label("vBot v".. version .." \n Vithrax#5814")
 UI.Button("Official OTCv8 Discord!", function() g_platform.openUrl("https://discord.gg/yhqBE4A") end)
