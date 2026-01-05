97a3c0b0f831cc881e008971035f860a791c63e1
divinity76 <hans@loltek.net>
2026-01-05 05:29:09 +0100

    oops forgot the macro

--- mods/game_bot/default_configs/vBot_4.8/vBot/main.lua
+++ mods/game_bot/default_configs/vBot_4.8/vBot/main.lua
@@ -5,17 +5,3 @@ local available = false
 UI.Label("vBot v".. version .." \n Vithrax#5814")
 UI.Button("Official OTCv8 Discord!", function() g_platform.openUrl("https://discord.gg/yhqBE4A") end)
 UI.Separator()
-
-schedule(5000, function()
-
-    if not available then return end
-    if currentVersion ~= version then
-
-        UI.Separator()
-        UI.Label("New vBot is available for download! v"..currentVersion)
-        UI.Button("Go to vBot GitHub Page", function() g_platform.openUrl("https://github.com/Vithrax/vBot") end)
-        UI.Separator()
-
-    end
-
-end)
