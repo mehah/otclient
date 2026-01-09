4e74ffb721760cc33ffb57fc081d2d0f4f7baf71
André Morais <andreoam@gmail.com>
2026-01-09 00:40:28 -0300

    commit

--- modules/gamelib/player.lua
+++ modules/gamelib/player.lua
@@ -273,3 +273,31 @@ end
 function Player:getVocationNameByClientId()
     return vocationNamesByClientId[self:getVocation()] or "Unknown Vocation"
 end
+
+if not LoadedPlayer then
+  LoadedPlayer = {
+    playerId = 0,
+    playerName = "",
+    playerVocation = 0,
+  }
+  LoadedPlayer.__index = LoadedPlayer
+end
+
+function LoadedPlayer:getId() return self.playerId end
+function LoadedPlayer:getName() return self.playerName end
+function LoadedPlayer:getVocation() return self.playerVocation end
+function LoadedPlayer:isLoaded()
+  return self.playerId > 0
+end
+
+function LoadedPlayer:setId(playerId)
+  self.playerId = playerId
+end
+
+function LoadedPlayer:setName(playerName)
+  self.playerName = playerName
+end
+
+function LoadedPlayer:setVocation(vocationId)
+  self.playerVocation = vocationId
+end
