63c1351a5bf47fc201bac84395e8c8751425a06e
LIBERGOD <37589902+libergod@users.noreply.github.com>
2026-01-23 11:46:33 -0800

    Update wasm32-emscripten-pthread.cmake

--- browser/triplets/wasm32-emscripten-pthread.cmake
+++ browser/triplets/wasm32-emscripten-pthread.cmake
@@ -3,6 +3,7 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Emscripten)
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "$ENV{EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
 
 # Force pthread support for all packages
 set(VCPKG_C_FLAGS "-pthread")
